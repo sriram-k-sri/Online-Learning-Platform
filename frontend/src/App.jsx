4c95d5ce04d04bf7708dd0eb3df7e588a7738dcd
sriram-k-sri <143984746+sriram-k-sri@users.noreply.github.com>
2024-11-14 15:11:34 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
           </div>
           <footer className="bg-light text-center text-lg-start">
             <div className="text-center p-3">
-              © {date} Copyright: Study App
+              © {date} Copyright: Course Cloud
             </div>
           </footer>
         </Router>
